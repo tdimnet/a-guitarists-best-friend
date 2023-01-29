8a918d575fb42ed21c674c17add8b917f4250bc5
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-01-29 20:09:01 +0100

    Now playing (and stopping) sound each second

--- App.js
+++ App.js
@@ -4,15 +4,27 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import { Audio } from 'expo-av'
 
 export default function App() {
+  const [ isTicking, setIsTicking ] = useState(false)
+  const [ seconds, setSeconds ] = useState(0)
+
+  useEffect(() => {
+    const timer = setTimeout(() => isTicking && setSeconds(seconds + 1), 1e3) && playSound()
+    return () => clearTimeout(timer)
+  }, [seconds, isTicking])
+
   async function playSound() {
     const { sound } = await Audio.Sound.createAsync(require('./assets/AccessGranted.m4a'))
     await sound.playAsync()
   }
 
+
   return (
     <View style={styles.container}>
       <Text>Hello, World!</Text>
+      <Text>Number of seconds is {seconds}</Text>
       <Button onPress={() => playSound()} title="Click me" />
+      <Button onPress={() => setIsTicking(false)} title="Pause" />
+      <Button onPress={() => setIsTicking(true)} title="Resume" />
       <StatusBar style="auto" />
     </View>
   );
