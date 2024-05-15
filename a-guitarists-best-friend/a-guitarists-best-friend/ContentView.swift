7786ca83b660ce27eb4f2c76638476177f675c78
Thomas Dimnet <thomas.dimnet@gmail.com>
2024-05-15 09:01:45 +0200

    Switching to iOS

---
+++ a-guitarists-best-friend/a-guitarists-best-friend/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  a-guitarists-best-friend
+//
+//  Created by family on 15/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ a-guitarists-best-friend/a-guitarists-best-friend/a_guitarists_best_friendApp.swift
@@ -0,0 +1,17 @@
+//
+//  a_guitarists_best_friendApp.swift
+//  a-guitarists-best-friend
+//
+//  Created by family on 15/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct a_guitarists_best_friendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
